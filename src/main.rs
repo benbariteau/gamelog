36537f9a874d9be10f275b1f2b0b626db8f688c9
Ben Bariteau <firba1@gmail.com>
2017-01-18 20:53:04 -0800

    add basic Hello World web stuff with iron

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+
 fn main() {
-    println!("Hello, world!");
+    Iron::new(|_: &mut Request| {
+        Ok(Response::with((status::Ok, "Hello World!")))
+    }).http("0.0.0.0:3000").unwrap();
 }
